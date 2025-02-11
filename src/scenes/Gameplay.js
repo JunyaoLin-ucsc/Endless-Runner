2e25853274898723d514624fc4df4782b8a6f1f8
JunyaoLin-ucsc <jlin232@ucsc.edu>
2025-02-10 22:29:21 -0800

    !!!

--- src/scenes/Gameplay.js
+++ src/scenes/Gameplay.js
@@ -129,7 +129,7 @@ class Gameplay extends Phaser.Scene {
         if (window.bgmSound && window.bgmSound.isPlaying) {
             window.bgmSound.stop();
         }
-        window.bgmSound = this.sound.add('bgm', { loop: true, volume: 0.25 });
+        window.bgmSound = this.sound.add('bgm', { loop: true, volume: 0.1 });
         window.bgmSound.play();
   
         this.time.addEvent({
